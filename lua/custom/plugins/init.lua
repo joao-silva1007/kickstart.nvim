b9408d24fbcfcac061c74dcd72a3213b1196b8d5
joaomsilva <joaomsilva@PT001792.Home>
2024-11-16 18:47:05 +0000

    changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { dir = '~/vim-krakend/' },
+  { 'tpope/vim-abolish' },
+}
